5c9a80f1397ed766b83604f1994ed31bf00a2304
Reawpai Chunsoi <rchunsoi@hflocal.com>
2026-02-10 12:01:29 +0700

    chore: update new node requirment

--- apps/web/app/layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="flex min-h-screen flex-col">
-    <AppHeader />
-    <main class="grow">
-      <slot />
-    </main>
-    <AppFooter />
-  </div>
-</template>
--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import configs from '@phaicom/eslint-config'
 export default
 configs(
   {
-    vue: true,
     tailwind: true,
     formatters: true,
     ignores: ['pnpm-workspace.yaml'],
