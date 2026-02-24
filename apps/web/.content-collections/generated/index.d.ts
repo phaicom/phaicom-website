32b92860219359dd24afa44e2af928a426d15779
Reawpai Chunsoi <reawpai.chun@yahoo.com>
2026-02-24 11:02:25 +0700

    chore: ignore content collection

--- apps/web/.content-collections/generated/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { GetTypeByName } from "@content-collections/core";
-
-import configuration from "../../content-collections.ts";
-
-export type Project = GetTypeByName<typeof configuration, "projects">;
-export declare const allProjects: Array<Project>;
